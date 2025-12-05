f295a6ee88fbe884efcf7d24e5d15bd31587b227
Dhiraj Kumbhre <131527754+dhirajkumbhre@users.noreply.github.com>
2025-12-05 20:43:07 +0530

    Update auth.controller.js

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let exists = await User.findOne({ email });
+  if (exists) return res.status(400).json({ message: "Email already used" });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+  });
+
+  res.json({ message: "User Registered", user });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ message: "Invalid credentials" });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.json({ token, user });
+};
